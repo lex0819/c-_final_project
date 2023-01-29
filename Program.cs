99aaeb4765694fe126b03516ad4bcddebdd6e56e
lex0819 <lexmolnar@gmail.com>
2023-01-29 12:11:56 +0300

    readme

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
 ﻿// тестовый массив
-string[] array01 = { "kolobok", "-2", ":))" };
+string[] array01 = { "hello", "2", "world", ":-)" };
+string[] array02 = { "1234", "1567", "-2", "computer sciennce" };
+string[] array03 = { "Russia", "Denmark", "Kazan" };
